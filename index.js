403231f31bf726472bb82e08161c8c021b493f03
Amanda <amandaforni125@gmail.com>
2020-11-16 18:25:05 +0000

    Game Model

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const  app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
 const User = require('./models/user');
+const Game = require('./models/game');
 const bodyParser = require('body-parser');
+const game = require('./models/game');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -22,8 +24,8 @@ app.get('/users', (req, res) => {
     User.find({}, (err, users) => {
         res.status(200);
         res.send(users);
-    })
-})
+    });
+});
 
 app.post('/users', (req, res) => {
     const user = new User({
@@ -33,8 +35,50 @@ app.post('/users', (req, res) => {
     user.save().then(() => {
         res.status(200);
         res.send({'status': 'worked', 'email': req.body.email, 'password': req.body.password});
+    });
+});
+
+app.get('/game', (req, res) => {
+    Game.find({}, (err, games) => {
+        res.status(200);
+        res.send(games);
+    });
+});
+
+app.post('/game', (req, res) => {
+    const game = new Game ({
+        title: req.body.title,
+        description: req.body.description,
+        pegi: req.body.pegi,
+        platform: req.body.platform,
+        ageRating: req.body.ageRating,
+        educationalValue: req.body.educationalValue,
+        easeOfPlay: req.body.easeOfPlay,
+        violence: req.body.violence,
+        sex: req.body.sex,
+        language: req.body.language,
+        consumerism: req.body.consumerism,
+        substanceUse: req.body.substanceUse
     })
-})
+    game.save().then(() => {
+        res.status(200);
+        res.send({'status':'worked', 
+            'title': req.body.title, 
+            'description': req.body.description, 
+            'pegi': req.body.pegi, 
+            'platform': req.body.platform,
+            'ageRating': req.body.ageRating,
+            'educationalValue': req.body.educationalValue,
+            'easeOfPlay': req.body.easeOfPlay,
+            'violence': req.body.violence,
+            'sex': req.body.sex,
+            'language': req.body.language,
+            'consumerism': req.body.consumerism,
+            'substanceUse': req.body.substanceUse 
+        });
+    });
+});
+
 
 app.listen(5000, () => {
     console.log('Listening on port 5000');
