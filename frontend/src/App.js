e8705e7b49c551bb13e4892f68cbfe7ae5b34d25
Joshua Rainbow <jrainbow1997@gmail.com>
2020-11-17 16:54:53 +0000

    Navbar links working!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import Navbar from './components/navbar/navBar';
 import Signup from './components/signup/signUp';
+import Main from './components/main/main';
+import Login from './components/login/login';
+import CreateReview from './components/createreview/createReview';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-      </header>
-      <body>
-      <Signup />
-      </body>
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <Navbar />
+          </header>
+          <Switch>
+            <Route exact path = "/" component = {Signup} />
+            <Route exact path = "/main" component = {Main} />
+            <Route exact path = "/login" component = {Login} />
+            <Route exact path = "/createReview" component = {CreateReview} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </React.Fragment>
   );
 }
 
