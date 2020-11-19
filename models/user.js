91cd8940db4495f18dbcce39c20121adf9c1ca8e
Martin Dev <u2001uk@yahoo.co.uk>
2020-11-19 10:31:53 +0000

    Models completed

--- models/user.js
+++ models/user.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     email:{type: String, required: true, unique: true},
     password:{type: String, required: true, minlength: 8}
-})
+});
 
 module.exports = mongoose.model('Users', userSchema);
