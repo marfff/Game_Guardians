6d6a54b0a65e871337d06a9925e10bc969289705
ecbaldocke612 <e.charlotte94@gmail.com>
2020-11-19 13:21:18 +0000

    Added express-session authorisation
    User now can login, logout and signup. Used function added in userSchema file to check password against mongodb and returns a new session if user has submitted the correct login details

--- models/user.js
+++ models/user.js
@@ -5,4 +5,20 @@ const userSchema = new mongoose.Schema({
     password:{type: String, required: true, minlength: 8}
 })
 
+userSchema.statics.checkExists = async function(email) {
+    return await this.exists({email});
+}
+    
+userSchema.statics.checkPassword = async function(email, password) {
+    const user = await this.findOne({email});
+    
+    if (!user) {
+        return false;
+    }
+    if(user.password == password) {
+        return true;
+    }
+    return false;
+}
+
 module.exports = mongoose.model('Users', userSchema);
