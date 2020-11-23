2995097de62628538716639e6ef625775df51e67
Amanda <amandaforni125@gmail.com>
2020-11-23 14:03:07 +0000

    CSS for UserPage

--- frontend/src/components/userPage/userPage.js
+++ frontend/src/components/userPage/userPage.js
@@ -2,17 +2,21 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import { Router, useHistory } from 'react-router-dom';
 import axios from 'axios'; 
-
+import './userPage.css'; 
 
 function UserPage() {
     return(
-        <div>
+        <div class="userBackground">
         {(sessionStorage.getItem("login")) ? 
             <div>
-            <h1>Welcome,Game Guardian!</h1>
+            <h1>Welcome, Game Guardian!</h1>
+            <br/>
+            <br/>
                 <p>Here you can find all you need to manage your account and reviews.</p>
                 <br/>
                 <p>My submitted reviews (find reviews with the email attached?)</p>
+                <br/>
+                <br/>
                 <br/>
                 <li><Link to="/deleteUser">Delete Account</Link></li>
             </div>
