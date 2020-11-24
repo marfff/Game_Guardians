0f5ca078503f7254bc1c7cf2b898d7dac885c1fe
ecbaldocke612 <e.charlotte94@gmail.com>
2020-11-24 13:58:47 +0000

    Logout looks pretty now
    Logs user out and button to take you back to the login page

--- frontend/src/components/logout/Logout.js
+++ frontend/src/components/logout/Logout.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-import '../signup/signUp.css';
+import { Link } from 'react-router-dom';
+import './logout.css';
 
 function Logout() {
 
     sessionStorage.removeItem("email");
     sessionStorage.removeItem("login");
 
-
     return(
-        <div>
-            <h1>YOU HAVE LOGGED OUT</h1>
+        <div class="logoutWrapper">
+            <h1>Successfully Logged Out</h1>
+            <h3>Goodbye for now!
+                <br/>
+                <br/>
+            Click below to navigate back to the login page.
+            </h3>
+            <a id="logoutLink"><Link to="/login">Log me back in!</Link></a>
         </div>
     );
 };
