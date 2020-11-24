d54d3aea012ed3059b9b564c61499c65127a9cbe
Amanda <amandaforni125@gmail.com>
2020-11-24 14:31:25 +0000

    UI for Games and Reviews Pages

--- frontend/src/components/game/gamesSummary.js
+++ frontend/src/components/game/gamesSummary.js
@@ -30,12 +30,13 @@ export default class Games extends React.Component{
 
         return (
             <div className="gamesList">
+            <h1 id="gamesTitle">Games</h1>
             <h2>{this.state.games.map((game, index) => {
                 return <ul><a href={`/game/${game._id}`} key={index}>
                             <li className="gamesLogo"><Images id = {game._id}/></li>
                             <li className="gamesListTitle">{game.title}</li>
-                            <li>{game.short_desc}</li>
-                            <li>PEGI: {game.pegi}</li>
+                            <li className="gamesDesc">{game.short_desc}</li>
+                            <li className="pegi">PEGI Rating: {game.pegi}</li>
                         </a></ul>
             })}</h2>
             </div>
